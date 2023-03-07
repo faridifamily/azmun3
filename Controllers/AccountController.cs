0a8631dba49c77bbf2b1847aa7c083d20ac15d15
faridifamily <124078542+faridifamily@users.noreply.github.com>
2023-03-07 18:35:29 +0330

    first commit

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,22 @@
+﻿using azmoooon.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace azmoooon.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(string mobile, string email)
+        {
+            var user = new User();
+            user.Email = email;
+            user.Mobile= mobile;
+            return View();
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace azmoooon.Models
+{
+    public class User
+    {
+
+        [Required(ErrorMessage = "ورود ایمیل اجباری است")]
+        public string Email { get; set; }
+
+        [MinLength(11, ErrorMessage = "طول موبایل باید 11 رقم باشد")]
+        [MaxLength(11, ErrorMessage = "طول موبایل باید 11 رقم باشد")]
+        public string Mobile { get; set; }
+    }
+}
